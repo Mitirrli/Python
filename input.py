76568be9c65ad1805e7d3ff394f97d4528a8cfc3
黄磊 <huangl@golive.cc>
2019-08-26 10:58:29 +0800

    计算两数和

--- input.py
+++ input.py
@@ -2,5 +2,10 @@
 # name = input()
 # print('Hello', name)
 
-name = input('请输入你的姓名 :')
-print('Hello', name)
+# name = input('请输入你的姓名 :')
+# print('Hello', name)
+
+# 计算加减
+firstNum = input('请输入第一个数 :')
+secondNum = input('请输入第二个数 :')
+print(int(firstNum) + int(secondNum))
