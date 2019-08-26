a7cd2d6ec410adb30d3e013dc086094f6374b30f
黄磊 <huangl@golive.cc>
2019-08-26 20:36:26 +0800

    调用函数

---
+++ function.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+print(abs(100))
+print(abs(-20))
+
+print(max(1, 2, 3, 4, 5))
+print(int('123'))
+print(str(123))
+
+# 匿名函数
+a = abs
+print(a(-1))
+
+n = 255
+print('%s' % hex(n))
