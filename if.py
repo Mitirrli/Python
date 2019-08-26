04d082486b9d767e9701b538ecc4bf47ae6081e0
黄磊 <huangl@golive.cc>
2019-08-26 17:07:08 +0800

    elif

---
+++ if.py
@@ -0,0 +1,35 @@
+# age = 20
+# if age >= 18:
+#     # print('你的年龄是%d' % age)
+#     print('你的年龄是', age)
+#     print('adult')
+
+
+# age = 3
+# if age >= 18:
+#     # print('你的年龄是%d' % age)
+#     print('你的年龄是', age)
+#     print('adult')
+# else:
+#     print('你的年龄是', age)
+#     print('teenager')
+
+
+age = 3
+if age >= 18:
+    # print('你的年龄是%d' % age)
+    print('你的年龄是', age)
+    print('adult')
+elif age == 3:
+    print('three')
+else:
+    print('你的年龄是', age)
+    print('teenager')
+
+
+# if x:
+#     print('True')
+# 只要x是非零数值、非空字符串、非空list等，就判断为True，否则为False
+
+
+
