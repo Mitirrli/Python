427aca3683f54fe1f97bd03c6f99cc513ad04731
黄磊 <huangl@golive.cc>
2019-08-26 13:44:40 +0800

    追加元素

--- list.py
+++ list.py
@@ -2,6 +2,13 @@ color = ['red', 'blue', 'yellow']
 print(color)
 print(len(color))
 print(color[0])
+
+# 取最后一个元素
 print(color[-1])
 
+# 取最后第二个元素
+print(color[-2])
 
+# list是可变的有序表,可以追加元素到末尾
+color.append('pink')
+print(color)
